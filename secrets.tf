cb68e61c691c8ede5acfdea450b728c9615abe4a
Otavio Andrade <otavio.sto@gmail.com>
2024-09-16 22:39:37 -0300

    conf: rename secret

--- secrets.tf
+++ secrets.tf
@@ -1,6 +1,6 @@
 # Criar o segredo no Secrets Manager
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "rds-postgres-credentials_v3"
+  name = "rds-postgres-credentials_v4"
 }
 
 # Armazenar as credenciais e o endpoint no Secrets Manager
