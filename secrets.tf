580b69670583874e2acfd6869e7485b781d22693
Otavio Andrade <otavio.sto@gmail.com>
2024-09-19 20:52:05 -0300

    conf: deploy

--- secrets.tf
+++ secrets.tf
@@ -1,6 +1,6 @@
 # Criar o segredo no Secrets Manager
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "rds-postgres-credentials_v6"
+  name = "rds-postgres-credentials_v7"
 }
 
 # Armazenar as credenciais e o endpoint no Secrets Manager
