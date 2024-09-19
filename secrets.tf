dbbe50864239db8b2b8b9c8a4da0c5256e2f1132
Otavio Andrade <otavio.sto@gmail.com>
2024-09-18 22:14:45 -0300

    conf: disable snapshot

--- secrets.tf
+++ secrets.tf
@@ -1,6 +1,6 @@
 # Criar o segredo no Secrets Manager
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "rds-postgres-credentials_v5"
+  name = "rds-postgres-credentials_v6"
 }
 
 # Armazenar as credenciais e o endpoint no Secrets Manager
@@ -8,8 +8,8 @@ resource "aws_secretsmanager_secret_version" "db_credentials" {
   secret_id     = aws_secretsmanager_secret.db_credentials.id
   secret_string = jsonencode({
     db_name   = var.db_name
-    username  = var.db_username
-    password  = var.db_password
-    endpoint  = aws_db_instance.postgres.endpoint
+    db_username  = var.db_username
+    db_password  = var.db_password
+    db_endpoint  = aws_db_instance.postgres.endpoint
   })
 }
