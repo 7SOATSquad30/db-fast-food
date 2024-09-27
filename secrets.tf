cc6ad2007d6c828a6c8a2df4c8b577141718beb0
Otavio Andrade <otavio.sto@gmail.com>
2024-09-27 01:21:30 -0300

    fix: correção versão

--- secrets.tf
+++ secrets.tf
@@ -1,10 +1,11 @@
 # Criar o segredo no Secrets Manager
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "rds-postgres-credentials_v7"
+  name = "rds-postgres-credentials"
 }
 
 # Armazenar as credenciais e o endpoint no Secrets Manager
 resource "aws_secretsmanager_secret_version" "db_credentials" {
+  depends_on = [aws_db_instance.postgres]
   secret_id     = aws_secretsmanager_secret.db_credentials.id
   secret_string = jsonencode({
     db_name   = var.db_name
