6fce55cbe5b1787f6f71d85f3612e4561c456c68
Otavio Andrade <otavio.sto@gmail.com>
2024-09-27 01:00:24 -0300

    fix: remoção das vpc, subnets

--- variables.tf
+++ variables.tf
@@ -4,36 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block da VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_1_cidr" {
-  description = "CIDR block da primeira Subnet"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "subnet_2_cidr" {
-  description = "CIDR block da segunda Subnet"
-  type        = string
-  default     = "10.0.2.0/24"
-}
-
-variable "availability_zone_1" {
-  description = "Zona de disponibilidade para a primeira Subnet"
-  type        = string
-  default     = "us-east-1a"
-}
-
-variable "availability_zone_2" {
-  description = "Zona de disponibilidade para a segunda Subnet"
-  type        = string
-  default     = "us-east-1b"
-}
-
 variable "allowed_cidr" {
   description = "CIDR block permitido para acessar o banco de dados"
   type        = string
